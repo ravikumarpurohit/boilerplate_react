4977aba987f0296215e63f22ab8d4b1becfcf413
Ravikumar Purohit <ravi23691y@gmail.com>
2025-02-20 11:09:30 +0530

    vite update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Specify output directory
+    sourcemap: false, // Disable sourcemaps for smaller builds
+    minify: "esbuild", // Use esbuild for faster minification
+  },
+});
